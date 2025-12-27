49a4644cc0a3286563d4c2dd3eb1acb18e89d223
Godfrey-ngd <NGD18256249656@outlook.com>
2025-12-27 15:24:06 +0800

    创建数据dao

---
+++ app/src/main/java/com/example/todolist/data/local/TodoDao.kt
@@ -0,0 +1,26 @@
+package com.example.todolist.data.local
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+@Dao
+interface TodoDao {
+    //获取所有任务，按时间排序
+    @Query("SELECT * FROM todo_table ORDER BY dueDate ASC")
+    fun getAllTodos(): Flow<List<TodoEntity>>
+
+    //根据ID获取待办事项详情
+    @Query("SELECT * FROM todo_table WHERE id = :id")
+    fun getTodoById(id: Int): TodoEntity?
+
+    // 插入新任务
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTodo(todo: TodoEntity)
+
+    // 更新任务（例如标记完成）
+    @Update
+    suspend fun updateTodo(todo: TodoEntity)
+
+    // 删除单个任务
+    @Delete
+    suspend fun deleteTodo(todo: TodoEntity)
+}
